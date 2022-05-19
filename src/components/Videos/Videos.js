9cefdf3ccfe1dee6268b8fc1e04ae04bbdad9517
Bliqui <ronlyred@gmail.com>
2022-05-20 00:39:10 +0200

    Add GET request processing

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -6,13 +6,31 @@ import './Videos.scss';
 export const Videos = () => {
     const [listedVideos, setListedVideos] = useState([]);
     const [searchValue, setSearchValue] = useState([]);
-    function handleSubmit(){};
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        ytReq.get('/search', {
+            params: {
+                q: searchValue
+            }
+        })
+            .then((response) => {
+                setListedVideos([]);
+                setListedVideos(prevState => [...prevState, response])
+                console.log(response)
+                console.log(listedVideos)
+            })
+    };
+    function handeInputChange(e) {
+        setSearchValue(e.target.value)
+    };
 
     return (
         <div className={'videos-body'}>
             <div className={'videos-wrapper'}>
-                <form className={'input-wrapper'} onSubmit={handleSubmit}>
-                    <input className={'search-input'} placeholder={'Search'}/>
+                <form className={'input-wrapper'} onSubmit={(e) => handleSubmit(e)}>
+                    <input className={'search-input'} value={searchValue} onChange={(e) => handeInputChange(e)}
+                           placeholder={'Search'}/>
                     <button className={'input-search-btn'}>Search</button>
                 </form>
             </div>
