f65a82bcc37546eef0f74719df55424321297788
Bliqui <ronlyred@gmail.com>
2022-05-20 00:21:02 +0200

    Add input

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {ytReq} from "../../APIs/ytRequest";
+import search from "../../assests/images/search.png";
+import './Videos.scss';
 
 export const Videos = () => {
+    const [listedVideos, setListedVideos] = useState([]);
+    const [searchValue, setSearchValue] = useState([]);
+    function handleSubmit(){};
+
     return (
-        <div>
-            videos are here
+        <div className={'videos-body'}>
+            <div className={'videos-wrapper'}>
+                <form className={'input-wrapper'} onSubmit={handleSubmit}>
+                    <input className={'search-input'} placeholder={'Search'}/>
+                    <button className={'input-search-btn'}>Search</button>
+                </form>
+            </div>
+
         </div>
     );
 };
