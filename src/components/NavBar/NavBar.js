e814cbfb18c491cabe49b51e0ad885157b703074
Bliqui <ronlyred@gmail.com>
2022-05-19 23:30:23 +0200

    Initial commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,26 @@
+import './NavBar.scss'
+import heart from '../../assests/images/white-heart.png'
+import search from '../../assests/images/search.png'
+
+export const NavBar = () => {
+    return (
+        <nav className={'navBar-body'}>
+            <div className={'navBar-wrapper'}>
+                <div className={'navBar-row'}>
+                    <div className={'navBar-items'}>
+                        <span className={'navBar-item-left'}>Videos</span>
+                        <span className={'navBar-item-right'}>Favorite videos <img className={'heart-icon'} src={heart} alt="heart"/></span>
+                    </div>
+                    <div className={'input-wrapper'}>
+                        <button className={'input-search-btn'}><img src={search} alt="search"/></button>
+                        <input className={'search-input'} placeholder={'Search'}/>
+                    </div>
+
+                    <div className={'login-out-wrapper'}>
+                        <button className={'login-out-button'}>Logout</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
