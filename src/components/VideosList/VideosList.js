7fb5bfe3428473fc5b5ae6c017f79b5defdafbc9
Bliqui <ronlyred@gmail.com>
2022-05-20 22:28:31 +0200

    Add VideosList component | component with response data, will display videos

---
+++ src/components/VideosList/VideosList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const VideosList = ({listedVideos}) => {
+    const videosList = listedVideos[0]
+
+    /*const video = videosList.map((e) => {
+        return e
+    })*/
+
+    return (
+        <div className={'videosBody'}>
+            <div className={'videosWrapper'}>
+
+            </div>
+        </div>
+    );
+};
