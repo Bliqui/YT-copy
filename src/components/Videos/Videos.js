94968f01d28335629fefd2d3ac1cdcae77dbcfb8
Bliqui <ronlyred@gmail.com>
2022-05-20 22:33:43 +0200

    Add loader, redirect

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,14 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {ytReq} from "../../APIs/ytRequest";
 import search from "../../assests/images/search.png";
 import './Videos.scss';
+import {Loader} from "../Loader/Loader";
+import {useNavigate} from "react-router-dom";
+import {VideosList} from "../VideosList/VideosList";
 
 export const Videos = () => {
     const [listedVideos, setListedVideos] = useState([]);
     const [searchValue, setSearchValue] = useState([]);
+    const [loader, setLoader] = useState(false);
+    const navigate = useNavigate();
 
     function handleSubmit(e) {
         e.preventDefault()
+
+        setLoader(true)
+
         ytReq.get('/search', {
             params: {
                 q: searchValue
@@ -16,25 +24,29 @@ export const Videos = () => {
         })
             .then((response) => {
                 setListedVideos([]);
-                setListedVideos(prevState => [...prevState, response])
-                console.log(response)
-                console.log(listedVideos)
+                setListedVideos(prevState => [...prevState, response.data])
             })
+            .catch(() => {
+                alert('Oops... something could have gone wrong.');
+                navigate('/');
+            })
+            .finally(() => setLoader(false));
     };
+
     function handeInputChange(e) {
         setSearchValue(e.target.value)
     };
 
     return (
-        <div className={'videos-body'}>
+        <div className={'videos-search-body'}>
             <div className={'videos-wrapper'}>
                 <form className={'input-wrapper'} onSubmit={(e) => handleSubmit(e)}>
                     <input className={'search-input'} value={searchValue} onChange={(e) => handeInputChange(e)}
                            placeholder={'Search'}/>
                     <button className={'input-search-btn'}>Search</button>
                 </form>
+                {loader ? <Loader/> : <VideosList listedVideos={listedVideos}/>}
             </div>
-
         </div>
     );
 };
