52b3f4ade0788248aec8b39e95123ee93b0db1bb
Bliqui <ronlyred@gmail.com>
2022-05-23 01:00:05 +0200

    Provide website responsiveness

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -9,7 +9,7 @@ export const Video = ({videoData}) => {
         <div className={'videoBody'}>
             <div className={'videoWrapper'}>
                 <div className={'video'} onClick={() => setModalStatus(true)}>
-                    <img src={videoData.snippet.thumbnails.medium.url} alt=""/>
+                    <img className={'videoThumbnail'} src={videoData.snippet.thumbnails.medium.url} alt=""/>
                     <div className={'videoInfo'}>
                         <div className={'videoTitle'}>{videoData.snippet.title}</div>
                         <div className={'videoChannelName'}>{videoData.snippet.channelTitle}</div>
--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,9 +1,10 @@
 export const VideoPlayer = ({videoId}) => {
     return(
-        <iframe width="100%"
-                height="100%"
-                src={`https://www.youtube.com/embed/${videoId}`}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+        <iframe
+            width="100%"
+            height="100%"
+            src={`https://www.youtube.com/embed/${videoId}`}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
         </iframe>
     )
 }
