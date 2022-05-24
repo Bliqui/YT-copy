c92cf293b1930f646400bc63e926670538bbb466
Bliqui <ronlyred@gmail.com>
2022-05-24 17:11:59 +0200

    Provide 2 routes | login and register routes for future account creation possibility

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {Videos} from "./components/Videos/Videos";
+import {RegisterPage} from "./Pages/RegisterPage";
+import {LoginPage} from "./Pages/LoginPage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,12 +14,11 @@ root.render(
         <Routes>
             <Route path={'/'} element={<App/>}>
                 <Route index element={<Videos/>}/>
+                <Route path={'/login'} element={<LoginPage/>}/>
+                <Route path={'/register'} element={<RegisterPage/>}/>
             </Route>
         </Routes>
     </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
