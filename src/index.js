4df81de865ca5cae23afa0e82550714371b90ec6
Bliqui <ronlyred@gmail.com>
2022-05-24 22:19:08 +0200

    Add Redux provider

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import '../src/assests/styles/global.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "../src/assests/styles/global.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {Videos} from "./components/Videos/Videos";
-import {RegisterPage} from "./Pages/RegisterPage";
-import {LoginPage} from "./Pages/LoginPage";
+import {RegisterPage} from "./pages/RegisterPage/RegisterPage";
+import {LoginPage} from "./pages/LoginPage/LoginPage";
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
+import "./firebase"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <Routes>
-            <Route path={'/'} element={<App/>}>
-                <Route index element={<Videos/>}/>
-                <Route path={'/login'} element={<LoginPage/>}/>
-                <Route path={'/register'} element={<RegisterPage/>}/>
-            </Route>
-        </Routes>
+        <Provider store={store}>
+            <Routes>
+                <Route path={'/'} element={<App/>}>
+                    <Route index element={<Videos/>}/>
+                    <Route path={'/login'} element={<LoginPage/>}/>
+                    <Route path={'/register'} element={<RegisterPage/>}/>
+                </Route>
+            </Routes>
+        </Provider>
     </BrowserRouter>
 );
 
