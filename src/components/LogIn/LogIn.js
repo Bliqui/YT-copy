cb88291fdd5fb2d33f094e44a154dab38b740e16
Bliqui <ronlyred@gmail.com>
2022-05-24 17:15:13 +0200

    Provide two components LogIn and LogOut | they will return forms with APIs logic

---
+++ src/components/LogIn/LogIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {LoginOutForm} from "../LoginOutForm/LoginOutForm";
+
+export const LogIn = () => {
+    const handleLogin = () => {
+
+    }
+
+    return (
+        <div>
+
+        </div>
+    );
+};
