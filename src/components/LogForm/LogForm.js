01de3a08bc27e71485c32e7a674f714346353fe3
Bliqui <ronlyred@gmail.com>
2022-05-24 22:08:30 +0200

    Update requesting component

--- src/components/LogForm/LogForm.js
+++ src/components/LogForm/LogForm.js
@@ -1,26 +1,35 @@
 import React, {useState} from 'react';
+import './LogForm.scss'
 
 export const LogForm = ({title, handleClick}) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
     return (
-        <div>
-            <input
-                type="email"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-            />
-            <input
-                type="password"
-                placeholder="password"
-                onChange={(e) => setPass(e.target.value)}
-                value={pass}
-            />
-            <button onClick={handleClick}>
-                {title}
-            </button>
+        <div className={'formBody'}>
+            <div className={'formWrapper'}>
+                <div className={'inputWrapper'}>
+                    <input
+                        className={'emailInput'}
+                        type="email"
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                </div>
+                <div className={'inputWrapper'}>
+                    <input
+                        className={'passwordInput'}
+                        type="password"
+                        placeholder="Password"
+                        onChange={(e) => setPass(e.target.value)}
+                        value={pass}
+                    />
+                </div>
+                <button className={'submitBtn'} onClick={() => handleClick(email, pass)}>
+                    {title}
+                </button>
+            </div>
         </div>
     );
 };
