091c265f72e9f6a9ad2a6c5d95a4ed7fbb70169a
Bliqui <ronlyred@gmail.com>
2022-05-24 22:13:53 +0200

    Add login logic | redux set_user action called here also

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import {LogForm} from "../LogForm/LogForm";
+import {useDispatch} from "react-redux";
+import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+import {app} from "../../firebase";
+import {useNavigate} from "react-router-dom";
 
 export const SignUp = () => {
-    return (
-        <div>
 
-        </div>
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const auth = getAuth(app);
+    const handleRegister = (email, password) => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(({user}) => {
+                dispatch({
+                    type: 'SET_USER',
+                    payload: {
+                        email: user.email,
+                        id: user.uid,
+                        token: user.accessToken,
+                    }
+                });
+                navigate('/');
+            })
+            .catch(console.error)
+    }
+    return (
+        <LogForm
+            className={'form'}
+            title={'Sign up'}
+            handleClick={handleRegister}
+        />
     );
 };
