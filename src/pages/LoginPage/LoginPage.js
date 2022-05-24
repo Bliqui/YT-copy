1649525e2a3813e6e02ca1807612f4096c3f7181
Bliqui <ronlyred@gmail.com>
2022-05-24 22:02:44 +0200

    Add new button mixin

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import {LogIn} from "../components/LogIn/LogIn";
 
 export const LoginPage = () => {
     return (
         <div>
             <span>Login</span>
-
+            <LogIn/>
             <p> or <NavLink to={'/register'}>register</NavLink></p>
         </div>
     );
--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import {SignUp} from "../components/SignUp/SignUp";
 
 export const RegisterPage = () => {
     return (
         <div>
             <span>Register</span>
-
+            <SignUp/>
             <p>Already have an account? <NavLink to={'/login'}>log in</NavLink></p>
         </div>
     );
