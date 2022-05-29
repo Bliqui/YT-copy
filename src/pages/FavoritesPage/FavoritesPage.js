0f872c2016b617804e821b6885efb35e85bdf9af
Bliqui <ronlyred@gmail.com>
2022-05-29 13:34:34 +0200

    Add page with favorite videos

--- src/pages/FavoritesPage/FavoritesPage.js
+++ src/pages/FavoritesPage/FavoritesPage.js
@@ -1,10 +1,14 @@
-import React, {useEffect} from 'react';
+import React, {useEffect} from "react";
+import "./FavoritesPage.scss"
 import {useAuth} from "../../hooks/useAuth";
 import {useNavigate} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {NavLink} from "react-router-dom";
 
 export const FavoritesPage = () => {
     const {isAuth} = useAuth();
     const navigate = useNavigate();
+    const favs = useSelector((state) => state.favoritesReducer)
 
     useEffect(() => {
         if (isAuth === false) {
@@ -12,9 +16,23 @@ export const FavoritesPage = () => {
         }
     }, [isAuth])
 
+    if (favs.length === 0) {
+        return (
+            <div className={'favoritesBody'}>
+                <div className={'favoritesEmptyWrapper'}>
+                    <div className={'favoritesEmptyContent'}>
+                        <p className={'emptyTitle'}>Your favorite videos list is empty.</p>
+                        <NavLink className={'emptyAdvice'} to={'/'}>It's time to find your favorite video...</NavLink>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     return (
         <div className={'favoritesBody'}>
-            Favorites page
+            <div className={'favoritesWrapper'}>
+                Favorites page
+            </div>
         </div>
     );
 };
