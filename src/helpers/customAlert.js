1413e1ba5438b4394a8b4d443b4f0f295904b701
Bliqui <ronlyred@gmail.com>
2022-05-29 14:01:53 +0200

    Add a folder with functions that will reduce the amount of code

---
+++ src/helpers/customAlert.js
@@ -0,0 +1,32 @@
+import {toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
+
+export const alert = () => toast.warn('Something went wrong...', {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+});
+
+export const error = (text) => toast.error(text, {
+    position: "top-center",
+    autoClose: 6000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+});
+
+export const ok = (text) => toast.success(text, {
+    position: "top-center",
+    autoClose: 1000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+});
