e83cf8f27c3b8568cae4ec1548d145c51bff2ca4
Bliqui <ronlyred@gmail.com>
2022-05-30 23:33:26 +0200

    Provide redux-persist library for saving users information to the local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,21 @@ import {createStore, combineReducers} from "redux";
 import {userReducer} from "./userReducer";
 import {favoritesReducer} from "./favoritesReducer";
 import {videosListReducer} from "./videosListReducer";
+import {persistStore, persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage"
 
-const rootReducer = combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const rootReducerMain = combineReducers({
     userReducer,
     favoritesReducer,
     videosListReducer,
 });
 
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const persistedReducer = persistReducer(persistConfig, rootReducerMain)
+
+export const store = createStore(persistedReducer , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export let persistor = persistStore(store);
