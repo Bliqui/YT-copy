5e3a7b48f414976d95f5fbc95b7c81cfc48f7b73
Bliqui <ronlyred@gmail.com>
2022-05-30 21:55:41 +0200

    Add new VideosList reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import {createStore, combineReducers} from "redux";
 import {userReducer} from "./userReducer";
 import {favoritesReducer} from "./favoritesReducer";
+import {videosListReducer} from "./videosListReducer";
 
 const rootReducer = combineReducers({
     userReducer,
-    favoritesReducer
+    favoritesReducer,
+    videosListReducer,
 });
 
 export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
