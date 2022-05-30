a681cbd169c96661c71ee713e9628121149f490a
Bliqui <ronlyred@gmail.com>
2022-05-30 23:35:39 +0200

    Provide redux-persist library for saving users information to the local storage

--- src/index.js
+++ src/index.js
@@ -12,20 +12,25 @@ import {store} from "./redux/store";
 import "./firebase";
 import {FavoritesPage} from "./pages/FavoritesPage/FavoritesPage";
 import {ToastContainer} from "react-toastify";
+import {PersistGate} from "redux-persist/integration/react";
+import {persistor} from "./redux/store";
+import {Loader} from "./components/Loader/Loader";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <Provider store={store}>
-            <Routes>
-                <Route path={'/'} element={<App/>}>
-                    <Route index element={<Videos/>}/>
-                    <Route path={'/login'} element={<LoginPage/>}/>
-                    <Route path={'/register'} element={<RegisterPage/>}/>
-                    <Route path={'/favorites'} element={<FavoritesPage/>}/>
-                </Route>
-            </Routes>
-            <ToastContainer position="top-center" />
+            <PersistGate loader={<Loader />} persistor={persistor}>
+                <Routes>
+                    <Route path={'/'} element={<App/>}>
+                        <Route index element={<Videos/>}/>
+                        <Route path={'/login'} element={<LoginPage/>}/>
+                        <Route path={'/register'} element={<RegisterPage/>}/>
+                        <Route path={'/favorites'} element={<FavoritesPage/>}/>
+                    </Route>
+                </Routes>
+                <ToastContainer position="top-center" />
+            </PersistGate>
         </Provider>
     </BrowserRouter>
 );
